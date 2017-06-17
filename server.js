44964355f958f6805b2ab6283342ee62b65db371
naukmamh <demonpro100@i.ua>
2017-06-17 16:15:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello World\n');
+  res.end('Hello World!!!\n');
   //outputs string with line end symbol
 }).listen(1337, "127.0.0.1");
 //sets port and IP address of the server
